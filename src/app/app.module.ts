60b9f882b986aacc84d60f3db9f23416c993eb52
fogking <fogkings@nate.com>
2017-09-26 23:06:32 +0900

    헤더 추가

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-
+import { FooterComponent } from './footer/footer.component';
 import { BodyComponent } from './body/body.component';
 import { HonorPipe } from './pipe/honor.pipe';
 import { MouseTrackZoneComponent } from './body/mouse-track-zone/mouse-track-zone.component';
@@ -14,13 +14,12 @@ import { AnotherLoggerService } from "./service/another-logger.service";
 import { LogLevel } from "./service/log-level.enum";
 import { LOG_LEVEL_TOKEN } from "./app.token";
 import { BlockGameComponent } from './body/block-game/block-game.component';
-import { FooterComponent } from './footer/footer.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-
     BodyComponent,
     HonorPipe,
     MouseTrackZoneComponent,
